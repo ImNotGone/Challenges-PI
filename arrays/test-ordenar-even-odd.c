fde30e9924af2a68433217d0d954b2f67cd89753
Gonzalo Martone <gmartone@itba.edu.ar>
2023-04-04 10:13:18 -0300

    feat: add ordenar-even-odd challenge

---
+++ arrays/test-ordenar-even-odd.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <assert.h>
+
+// Dado un arreglo de elementos enteros,
+// reordenarlo poniendo todos los numeros pares al inicio (y los impares al final)
+
+// Retornar en res cualquier arreglo que cumpla la condicion
+// Por ejemplo: si el array de entrada es {3, 1, 2, 4}
+
+// Las soluciones validas serian
+//  1) {2, 4, 1, 3}
+//  2) {2, 4, 3, 1}
+//  3) {4, 2, 1, 3}
+//  4) {4, 2, 3, 1}
+
+
+// @param nums: array de numeros a ordenar
+// @param dim: dimension del array
+// @param res: array de salida
+void order_even_uneven(int nums[], int dim, int res[]);
+
+int main() {
+    int nums[] = {3, 1, 2, 4};
+    int dim = 4;
+    int res[4];
+
+    order_even_uneven(nums, dim, res);
+
+    int i = 0;
+    int expected_even = 2;
+    for (int i = 0; i < expected_even; i++) {
+        assert(res[i] % 2 == 0);
+    }
+
+    for (; i < dim; i++) {
+        assert(res[i] % 2 == 1);
+    }
+
+    puts("OK!");
+    return 0;
+}
+
+// @param nums: array de numeros a ordenar
+// @param dim: dimension del array
+// @param res: array de salida
+void order_even_uneven(int nums[], int dim, int res[]) {
+
+}
