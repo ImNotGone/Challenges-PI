67364f101df3d654cb9272ee45a399d34561a8f3
Gonzalo Martone <gmartone@itba.edu.ar>
2023-05-29 18:18:09 -0300

    chore: sol & test for rotate list

--- lists/sol-rotate-list.c
+++ lists/sol-rotate-list.c
@@ -46,6 +46,7 @@ Partiendo de una lista 5 -> 4 -> 3 -> 2 -> 1 -> NULL
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct node *TList;
 
@@ -122,40 +123,11 @@ TList rotate_list(TList list, unsigned int times)
     // ahora tengo que hacer que el ultimo nodo apunte al primero, uniendo los extremos
     append_list(new_tail, list);
 
-    return new_tail;    
-}
-
-static void print_rotation(unsigned int n) {
-    TNode n1 = {1, NULL};
-    TNode n2 = {2, &n1};
-    TNode n3 = {3, &n2};
-    TNode n4 = {4, &n3};
-    TNode n5 = {5, &n4};
-
-    TList list = &n5;
-
-    TList rotated = rotate_list(list, n);
-
-    printf("Rotated %d times:\n", n);
-    while (rotated != NULL)
-    {
-        printf("%d\n", rotated->elem);
-        rotated = rotated->tail;
-    }
-
-    puts("#####################");
+    return new_tail;
 }
 
 int main()
 {
-    print_rotation(0); // no deberia rotar
-    print_rotation(1); // 1 -> 5 -> 4 -> 3 -> 2
-    print_rotation(2); // 2 -> 1 -> 5 -> 4 -> 3
-    print_rotation(3); // 3 -> 2 -> 1 -> 5 -> 4
-    print_rotation(4); // 4 -> 3 -> 2 -> 1 -> 5
-    print_rotation(5); // no deberia rotar
-    print_rotation(6); // 1 -> 5 -> 4 -> 3 -> 2
-
     TNode n1 = {1, NULL};
     TNode n2 = {2, &n1};
     TNode n3 = {3, &n2};
@@ -167,23 +139,62 @@ int main()
     TList rotated = rotate_list(list, 2);
     TList rotated_copy = rotated;
 
+    TNode n1_aux_4_2rot = {3, NULL};
+    TNode n2_aux_4_2rot = {4, &n1_aux_4_2rot};
+    TNode n3_aux_4_2rot = {5, &n2_aux_4_2rot};
+    TNode n4_aux_4_2rot = {1, &n3_aux_4_2rot};
+    TNode n5_aux_4_2rot = {2, &n4_aux_4_2rot};
+
+    TList expected_for_2_rotations = &n5_aux_4_2rot;
+
     while (rotated != NULL)
     {
-        printf("%d\n", rotated->elem);
+        assert(expected_for_2_rotations->elem == rotated->elem);
         rotated = rotated->tail;
+        expected_for_2_rotations = expected_for_2_rotations->tail;
     }
 
-    puts("#####################");
+    assert(expected_for_2_rotations == NULL);
+
+    rotated = rotate_list(rotated_copy, 2);
+    rotated_copy = rotated;
+
+    TNode n1_aux_4_4rot = {5, NULL};
+    TNode n2_aux_4_4rot = {1, &n1_aux_4_4rot};
+    TNode n3_aux_4_4rot = {2, &n2_aux_4_4rot};
+    TNode n4_aux_4_4rot = {3, &n3_aux_4_4rot};
+    TNode n5_aux_4_4rot = {4, &n4_aux_4_4rot};
+
+    TList expected_for_4_rotations = &n5_aux_4_4rot;
+
+    while (rotated != NULL)
+    {
+        assert(expected_for_4_rotations->elem == rotated->elem);
+        rotated = rotated->tail;
+        expected_for_4_rotations = expected_for_4_rotations->tail;
+    }
+
+    assert(expected_for_4_rotations == NULL);
 
     rotated = rotate_list(rotated_copy, 2);
 
+    TNode n1_aux_4_6rot = {2, NULL};
+    TNode n2_aux_4_6rot = {3, &n1_aux_4_6rot};
+    TNode n3_aux_4_6rot = {4, &n2_aux_4_6rot};
+    TNode n4_aux_4_6rot = {5, &n3_aux_4_6rot};
+    TNode n5_aux_4_6rot = {1, &n4_aux_4_6rot};
+
+    TList expected_for_6_rotations = &n5_aux_4_6rot;
+
     while (rotated != NULL)
     {
-        printf("%d\n", rotated->elem);
+        assert(expected_for_6_rotations->elem == rotated->elem);
         rotated = rotated->tail;
+        expected_for_6_rotations = expected_for_6_rotations->tail;
     }
 
-    puts("#####################");
+    assert(expected_for_4_rotations == NULL);
 
+    puts("OK!");
     return 0;
 }
